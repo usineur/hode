baa8cd492f1fc84b7e357df3b1f8389bfe21ca77
CommonLoon102 <CommonLoon102@users.noreply.github.com>
2021-04-11 20:39:37 +0200

    move macro code to compat.h

--- compat.h
+++ compat.h
@@ -5,3 +5,17 @@
 #ifdef _MSC_VER
 #define PACK( __Declaration__ ) __pragma( pack(push, 1) ) __Declaration__ __pragma( pack(pop))
 #endif
+
+#ifdef _MSC_VER 
+// not #if defined(_WIN32) || defined(_WIN64)
+// because we have strncasecmp in mingw
+#define strncasecmp _strnicmp
+#define strcasecmp _stricmp
+#define MAXPATHLEN MAX_PATH
+#endif
+
+#if defined __WIN32__ || defined _WIN32 || defined _Windows
+#if !defined S_ISDIR
+#define S_ISDIR(m) (((m) & _S_IFDIR) == _S_IFDIR)
+#endif
+#endif
--- fs_posix.cpp
+++ fs_posix.cpp
@@ -10,15 +10,7 @@
 
 #include "fs.h"
 #include "util.h"
-
-#ifdef _MSC_VER 
-// not #if defined(_WIN32) || defined(_WIN64)
-// because we have strncasecmp in mingw
-#define strncasecmp _strnicmp
-#define strcasecmp _stricmp
-#define MAXPATHLEN MAX_PATH
-#endif
-
+#include "compat.h"
 
 static const char *_suffixes[] = {
 	"hod.dem",
