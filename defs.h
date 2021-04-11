f60cd3bc3ed827c910abfcc3ca972878e13f894a
CommonLoon102 <CommonLoon102@users.noreply.github.com>
2021-04-11 20:28:34 +0200

    remove duplicated code

--- defs.h
+++ defs.h
@@ -6,13 +6,7 @@
 #ifndef DEFS_H__
 #define DEFS_H__
 
-#ifdef __GNUC__
-#define PACK( __Declaration__ ) __Declaration__ __attribute__((__packed__))
-#endif
-
-#ifdef _MSC_VER
-#define PACK( __Declaration__ ) __pragma( pack(push, 1) ) __Declaration__ __pragma( pack(pop))
-#endif
+#include "compat.h"
 
 enum {
 	kPosTopScreen    = 0,
--- menu.h
+++ menu.h
@@ -2,15 +2,8 @@
 #ifndef MENU_H__
 #define MENU_H__
 
-#ifdef __GNUC__
-#define PACK( __Declaration__ ) __Declaration__ __attribute__((__packed__))
-#endif
-
-#ifdef _MSC_VER
-#define PACK( __Declaration__ ) __pragma( pack(push, 1) ) __Declaration__ __pragma( pack(pop))
-#endif
-
 #include "intern.h"
+#include "compat.h"
 
 struct Game;
 struct PafPlayer;
